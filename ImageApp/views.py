022f03e904813994ca72ba9c1be1842dbba253c3
Muhammad Ali Abbas <vynz420@gmail.com>
2021-04-02 20:57:52 -0400

    Update views.py
    Disabled Tensorflow

--- ImageApp/views.py
+++ ImageApp/views.py
@@ -10,7 +10,7 @@ from .models import Images,Backgrounds,Images_DB
 from django.conf import settings
 import os 
 import pixellib
-from pixellib.semantic import semantic_segmentation
+# from pixellib.semantic import semantic_segmentation
 import PIL
 from PIL import Image
 import numpy as np 
@@ -23,7 +23,7 @@ from copy import deepcopy
 from django.http import JsonResponse
 segment_image = semantic_segmentation()
 path=(settings.MEDIA_ROOT)
-segment_image.load_pascalvoc_model(os.path.join(path,"deeplabv3_xception_tf_dim_ordering_tf_kernels_improved.h5"))
+# segment_image.load_pascalvoc_model(os.path.join(path,"deeplabv3_xception_tf_dim_ordering_tf_kernels_improved.h5"))
 import random
 import base64
 import uuid
@@ -66,36 +66,37 @@ def check_mask_exists(fg):
 # Create your views here.
 def make_mask2(img_name):
     try:
-        file_name=os.path.basename(img_name)
-        img_path=os.path.join(path,'uploads')
-        mask_path=os.path.join(path,'masks')
-        a=segment_image.segmentAsPascalvoc(os.path.join(img_path,file_name), output_image_name = os.path.join(mask_path,file_name))
-        img=cv2.imread(os.path.join(img_path,file_name))
-        mask=cv2.imread(os.path.join(mask_path,file_name),0)
-        (thresh, mask) = cv2.threshold(mask, 130, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        kernel = np.ones((3,3), np.uint8)
-        mask = cv2.erode(mask,kernel,iterations = 3)
-        cv2.imwrite(os.path.join(mask_path,file_name),mask)
+#         file_name=os.path.basename(img_name)
+#         img_path=os.path.join(path,'uploads')
+#         mask_path=os.path.join(path,'masks')
+#         #a=segment_image.segmentAsPascalvoc(os.path.join(img_path,file_name), output_image_name = os.path.join(mask_path,file_name))
+#         img=cv2.imread(os.path.join(img_path,file_name))
+#         mask=cv2.imread(os.path.join(mask_path,file_name),0)
+#         (thresh, mask) = cv2.threshold(mask, 130, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+#         kernel = np.ones((3,3), np.uint8)
+#         mask = cv2.erode(mask,kernel,iterations = 3)
+#         cv2.imwrite(os.path.join(mask_path,file_name),mask)
         return {'msg':'Successful','file':file_name}
     except Exception as e:
         print(e)
         return {'msg':e,'file':None}
 def make_mask(img_name):
-    try:
-        file_name=os.path.basename(img_name)
-        img_path=os.path.join(path,'uploads')
-        mask_path=os.path.join(path,'masks')
-        a=segment_image.segmentAsPascalvoc(os.path.join(img_path,file_name), output_image_name = os.path.join(mask_path,file_name))
-        img=cv2.imread(os.path.join(img_path,file_name))
-        mask=cv2.imread(os.path.join(mask_path,file_name),0)
-        (thresh, mask) = cv2.threshold(mask, 130, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        kernel = np.ones((3,3), np.uint8)
-        mask = cv2.erode(mask,kernel,iterations = 3)
-        cv2.imwrite(os.path.join(mask_path,file_name),mask)
-        return 'Successful'
-    except Exception as e:
-        print(e)
-        return e
+#     try:
+#         file_name=os.path.basename(img_name)
+#         img_path=os.path.join(path,'uploads')
+#         mask_path=os.path.join(path,'masks')
+#         a=segment_image.segmentAsPascalvoc(os.path.join(img_path,file_name), output_image_name = os.path.join(mask_path,file_name))
+#         img=cv2.imread(os.path.join(img_path,file_name))
+#         mask=cv2.imread(os.path.join(mask_path,file_name),0)
+#         (thresh, mask) = cv2.threshold(mask, 130, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+#         kernel = np.ones((3,3), np.uint8)
+#         mask = cv2.erode(mask,kernel,iterations = 3)
+#         cv2.imwrite(os.path.join(mask_path,file_name),mask)
+#         return 'Successful'
+#     except Exception as e:
+#         print(e)
+#         return e
+        return 'error'
 def make_shadow(fg,bg,mask):
     try:
         img=fg
@@ -128,7 +129,7 @@ def change_bg_2(fg,bg):
             print(mask_fg)
             msg['msg']='Successful'
         else:
-            msg=make_mask2(fg)
+#             msg=make_mask2(fg)
           
             file_hash=hashlib.md5(open(fg,'rb').read()).hexdigest()
             mask_fg=msg['file']
@@ -197,40 +198,14 @@ def two_bg_change(request):
             bg=os.path.join(path,'uploads',os.path.basename(str(bg_image.image)))
             print(fg)
             print(bg)
-            data=change_bg_2(fg,bg)
+            data={'a':change_bg_2(fg,bg)}
             
         return JsonResponse(data)
         # bg=os.path.basename(bg)
     else: 
         print('Hhahha')
     return render(request,'ImageApp/upload1.html')
-def change_bg(request,fg,bg):
-    try:
-        output=fg=os.path.basename(fg)
-        bg=os.path.basename(bg)
-        img_path=os.path.join(path,'uploads')
-        mask_path=os.path.join(path,'masks')
-        bg_path=os.path.join(path,'background_pics')
-        output_path=os.path.join(path,'output')
-        print(img_path,mask_path,bg_path,output_path)
-        mask=cv2.imread(os.path.join(mask_path,fg),0)
-        (thresh, mask) = cv2.threshold(mask, 130, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        fg=cv2.imread(os.path.join(img_path,fg))
-        bg=cv2.imread(os.path.join(bg_path,bg))
-        bg=cv2.resize(bg,(fg.shape[1],fg.shape[0]))
-        bg=make_shadow(fg,bg,mask)
-        res_fg=cv2.bitwise_and(fg,fg,mask = mask)
-        res_bg = cv2.bitwise_and(bg,bg,mask = cv2.bitwise_not(mask))
-        new_image=res_fg+res_bg
-        unique_filename = str(uuid.uuid4())+'.jpg'
-        cv2.imwrite(os.path.join(output_path,unique_filename),new_image)
-        domain = request.get_host()
-        data={'MSG':'Success','image_url':'/media/output/'+unique_filename}
-        return data
-    except Exception as e:
-        bg=os.path.basename(bg)
-        data={'MSG':e,'image_url':'media/background_pics/'+output}
-        return data
+
     
 def home(request):
     return render(request,'ImageApp/home.html',{})
@@ -262,24 +237,7 @@ def ImageBackChanger(fg_image,bg_image):
     # Here you have user image primary key and background image primary key.
     # retrieve them from database.
     print("Integrate your code here please")
-@login_required
-def change_bg_view(request):
-    if request.method == 'POST':
-        back_id=request.POST.get('back_id')
-        back_image=Backgrounds.objects.get(id=back_id)
-        back_image=str(back_image.back_img)
-        front_image=Images.objects.filter(user_name=request.user.id).latest('id')
-        front_image=str(front_image.image)
-        msg=change_bg(request,front_image,back_image)
-        return HttpResponse(json.dumps(msg), content_type="application/json")
-    if request.method == 'GET':
-        data = {
-                'name': 'Vitor 1',
-                'location': 'Finland',
-                'is_active': True,
-                'count': 29
-        }
-        return JsonResponse(data)
+
 @login_required
 def file_upload_view(request):
     #print(len(request.FILES))
